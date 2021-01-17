b211b973da11d4ecaa083a4b8942e5d08434ffad
Robin <robin.lebert@gmail.com>
2021-01-17 15:21:02 +0100

    chore: initiate strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7f92a44b8cc381739dd64ac0bcfd346a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9fa08b01-3159-4931-8eff-799a2103b82d'
+};
